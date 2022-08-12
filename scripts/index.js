876fb7403d4af134b5fb6878c0608109957dab6a
Nigraum <yan.corintima@hotmail.com>
2022-08-12 17:27:11 -0300

    Starting to writer script

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+const slides = document.querySelectorAll('.slides');
+let slider = document.querySelector('.slider');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+let i = 0;
+
+function activeSlide(n){
+  for(slider of slides)
+  slider.classList.remove('active');
+  slides[n].classList.add('active');
+}
+
+function previous() {
+  prev.addEventListener('click');
+  if (i == slides.length - 1) {
+    i = 0;
+    activeSlide(i);
+  }
+  else {
+    i++;
+    activeSlide(i);
+  }
+};
